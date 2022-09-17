ce8f9a4df1a04ce91a66d4db1b35e0e2890f2558
Hei Kern Leong <heikern@hotmail.com>
2022-09-17 16:11:45 +0200

    add api

---
+++ api.py
@@ -0,0 +1,61 @@
+# from urllib.request import Request
+# from flask import request, jsonify
+# import flask
+
+import flask
+from flask import request, jsonify
+from joblib import dump, load
+from pathlib import Path
+from scipy.stats import norm
+
+curr_dir = Path.cwd()
+parent_dir = curr_dir.parents[0]
+output_data_dir = parent_dir / "machine_learning_model"
+model_path = output_data_dir / "model.joblib"
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Welcom to Concrete Concrete!!</h1>"
+
+
+@app.route('/get_quote', methods = ['GET'])
+def get_strength():
+    if 'wcr' in request.args and 'acr' in request.args:
+        wcr = request.args["wcr"]
+        acr = request.args["acr"]
+        long = request.args["long"]
+        lat = request.args["lat"]
+        req_strength = request.args["req_strength"]
+        pour_revenue = request.args["pour_revenue"]
+        
+        pour_star = request.args["pour_star"]
+
+        pour_star_multipliers = [1.2,1,0.9,0.8,0.5]
+        pour_star_coeff = pour_star_multipliers[pour_star]
+
+        clf = load(model_path)
+        mean_strength = float(clf.predict([[wcr,acr,long,lat]]))
+        # internal hardcoded parameter
+        sigma = 0.1
+        margin = 1.1
+        cdf = norm.cdf(x=req_strength, loc=mean_strength, scale=sigma)
+
+        premium = pour_revenue * margin * cdf * pour_star_coeff
+
+
+        output = {
+            "strength": mean_strength,
+            "premium": premium
+        }
+        return jsonify(output)
+    else:
+        return "gimmie something to work with"
+
+app.run()
+
+
+# http://127.0.0.1:5000/get_quote?wcr=0.45&acr=4.4&long=30&lat=50&&output=json
